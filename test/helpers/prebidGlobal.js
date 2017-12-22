4240d2b13d09a7a1439af74259c017d10eecfaf5
Manasi Moghe <manasi.moghe@pubmatic.com>
2017-12-22 14:52:06 +0530

    fixed jslinting errors

--- test/helpers/prebidGlobal.js
+++ test/helpers/prebidGlobal.js
@@ -1,8 +1,8 @@
-//window.$$PREBID_GLOBAL$$ = (window.$$PREBID_GLOBAL$$ || {});
-//window.$$PREBID_GLOBAL$$.cmd = window.$$PREBID_GLOBAL$$.cmd || [];
-//window.$$PREBID_GLOBAL$$.que = window.$$PREBID_GLOBAL$$.que || [];
+// window.$$PREBID_GLOBAL$$ = (window.$$PREBID_GLOBAL$$ || {});
+// window.$$PREBID_GLOBAL$$.cmd = window.$$PREBID_GLOBAL$$.cmd || [];
+// window.$$PREBID_GLOBAL$$.que = window.$$PREBID_GLOBAL$$.que || [];
 
-window.preBidNameSpace = "pbjs";
+window.preBidNameSpace = 'pbjs';
 $$PREBID_GLOBAL$$ = ($$PREBID_GLOBAL$$ || {});
 $$PREBID_GLOBAL$$.cmd = $$PREBID_GLOBAL$$.cmd || [];
 $$PREBID_GLOBAL$$.que = $$PREBID_GLOBAL$$.que || [];
