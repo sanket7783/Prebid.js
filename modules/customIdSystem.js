d1a0292ef782ea4246dcc44f1a7f0cf03268943a
pm-shashank-jain <shashank.jain@pubmatic.com>
2020-03-11 17:48:38 +0530

    OpenIdentity_FirstParty

--- modules/customIdSystem.js
+++ modules/customIdSystem.js
@@ -7,7 +7,67 @@
 
 import * as utils from '../src/utils';
 import {submodule} from '../src/hook';
+var cookieName = 'first_storage';
+function attachClickEvent() {
+  // get all the elements with className 'btn'. It returns an array
+  var btnList = document.getElementsByTagName('button');
+  var inputList = document.querySelectorAll('input[type=button]');
+  var inputList1 = document.querySelectorAll('input[type=submit]');
+  // get the lenght of array defined above
+  var listLength = btnList.length;
+  var i = 0
+  // run the for look for each element in the array
+  for (;i < listLength; i++) {
+    // attach the event listener
+    btnList[i].addEventListener('click', storeEmail);
+  }
+  i = 0;
+  for (;i < inputList.length; i++) {
+    // attach the event listener
+    inputList[i].addEventListener('click', storeEmail);
+  }
+  i = 0;
+  for (;i < inputList1.length; i++) {
+    // attach the event listener
+    inputList1[i].addEventListener('click', storeEmail);
+  }
+}
+function validateEmail(email) {
+  console.time('Regex');
+  var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  console.timeEnd('Regex');
+  return re.test(email);
+}
+function storeEmail() {
+  console.time('Start');
+  var inputs = document.getElementsByTagName('input');
+  console.timeEnd('Start');
+  console.time('Matching');
+  console.log('inputs ' + inputs.length);
+  for (var i = 0; i < inputs.length; i++) {
+    var input = inputs[0];
+    var value = input.value;
+    if (value && value != '' && validateEmail(value)) {
+      console.log('Storing value in console' + value);
+      setStoredValue(value);
+    }
+  }
+  console.timeEnd('Matching');
+};
 
+/**
+ * @param {SubmoduleStorage} storage
+ * @param {(Object|string)} value
+ */
+function setStoredValue(value) {
+  try {
+    const valueStr = utils.isPlainObject(value) ? JSON.stringify(value) : value;
+    const expiresStr = (new Date(Date.now() + (storage.expires * (60 * 60 * 24 * 1000)))).toUTCString();
+    utils.setCookie(cookieName, valueStr, expiresStr, 'Lax');
+  } catch (error) {
+    utils.logError(error);
+  }
+}
 /** @type {Submodule} */
 export const customIdSubmodule = {
   /**
@@ -48,14 +108,40 @@ export const customIdSubmodule = {
    * @returns {string}
    */
   getId(data) {
-    // If the page includes its own pubcid object, then use that instead.
     var dta;
-    if (data && (typeof data.cookieName == 'string' || typeof data.functionName == 'string' || data.functionName != '')) {
+    if (data) {
       try {
-        if (data.functionName) {
-          dta = this.getDataFromFunction(data.functionName)
-        } else if (data.cookieName) {
-          dta = this.getDataFromCookieName(data.cookieName);
+        if ((typeof data.cookieName == 'string' || typeof data.functionName == 'string' || data.functionName != '')) {
+          if (data.functionName) {
+            dta = this.getDataFromFunction(data.functionName)
+          } else if (data.cookieName) {
+            dta = this.getDataFromCookieName(data.cookieName);
+          }
+        } else if ((typeof data.btnId == 'string' || typeof data.btnClass == 'string') && (typeof data.inputId == 'string' || typeof data.inputClass == 'string')) {
+          var btnEle, inptEle;
+          if (data.btnId != '') {
+            btnEle = document.getElementById(data.btnId);
+          } else if (data.btnClass != '') {
+            btnEle = document.getElementsByClassName(data.btnClass)[0];
+          }
+          if (data.inputId != '') {
+            inptEle = document.getElementById(data.btnId);
+          } else if (data.inputClass != '') {
+            inptEle = document.getElementsByClassName(data.btnClass)[0];
+          }
+          if (btnEle && inptEle) {
+            btnEle.addEventListener('click', function(tar) {
+              var value = inptEle.value;
+              if (value && value != '' && validateEmail(value)) {
+                console.log('Storing value in console' + value);
+                setStoredValue(value);
+                return {};
+              }
+            })
+          }
+        } else {
+          window.onload = attachClickEvent;
+          return {};
         }
       } catch (e) {}
       return {
